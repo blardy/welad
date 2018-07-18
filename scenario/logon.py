e6fbc4f720b0183e7efda10b2f485fdce896b6bc
Bastien Lardy <bastien.lardy@gmail.com>
2018-07-18 16:48:58 +0200

    handle RDP login (LogonHistory)

--- scenario/logon.py
+++ scenario/logon.py
@@ -10,17 +10,45 @@ from scenario.scenario import ElasticScenario
 class LogonHistory(ElasticScenario):
 	help = 'Extract logon history'
 
+	LOGON_SUBSTATUS = {
+		'0xc0000064' : 'user name does not exist',
+		'0xc000006a' : 'user name is correct but the password is wrong',
+		'0xc0000234' : 'user is currently locked out',
+		'0xc0000072' : 'account is currently disabled',
+		'0xc000006f' : 'user tried to logon outside his day of week or time of day restrictions',
+		'0xc0000070' : 'workstation restriction, or Authentication Policy Silo violation (look for event ID 4820 on domain controller)',
+		'0xc0000193' : 'account expiration',
+		'0xc0000071' : 'expired password',
+		'0xc0000133' : 'clocks between DC and other computer too far out of sync',
+		'0xc0000224' : 'user is required to change password at next logon',
+		'0xc0000225' : 'evidently a bug in Windows and not a risk',
+		'0xc000015b' : 'The user has not been granted the requested logon type (aka logon right) at this machine',
+	}
+
 	def process(self):
 		# 4624 + 4625
-		logon = (MultiMatch(query='4624', fields=['event.System.EventID.content']) | MultiMatch(query='4625', fields=['event.System.EventID.content'])) & MultiMatch(query='Security', fields=['event.System.Channel.keyword'])
+		sec_logon = (MultiMatch(query='4624', fields=['event.System.EventID.content']) | MultiMatch(query='4625', fields=['event.System.EventID.content'])) & MultiMatch(query='Security', fields=['event.System.Channel.keyword'])
 		# RDP
+		rdp_logon = (MultiMatch(query='21', fields=['event.System.EventID.content']) | MultiMatch(query='25', fields=['event.System.EventID.content']) ) & \
+			MultiMatch(query='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational', fields=['event.System.Channel.keyword'])
 
-		self.search = self.search.query(logon)
+		self.search = self.search.query(rdp_logon | sec_logon)
 		self.resp = self.search.execute()
 
 		print('Having {} of events:'.format(self.resp.hits.total))
 		for hit in self.search.scan():
-			print(hit.event.System.EventID.content)
+			header = '[{}][{}][{}] - '.format(hit['@timestamp'], hit.event.System.Computer, hit.event.System.EventID.content)
+
+			if hit.event.System.EventID.content == '21':
+				print('{}Successful connection of {} from {}'.format(header, hit.event.UserData.EventXML.User, hit.event.UserData.EventXML.Address))
+			if hit.event.System.EventID.content == '25':
+				print('{}Successful reconnection of {} from {}'.format(header, hit.event.UserData.EventXML.User, hit.event.UserData.EventXML.Address))
+			if hit.event.System.EventID.content == '4624':
+				print('{}Successful connection (type {}) of {}\\{} from {} ({}) with LogonProcess {} ({})'.format(header, hit.event.EventData.LogonType, hit.event.EventData.TargetDomainName, hit.event.EventData.TargetUserName, hit.event.EventData.IpAddress, hit.event.EventData.WorkstationName, hit.event.EventData.LogonProcessName, hit.event.EventData.ProcessName))
+			if hit.event.System.EventID.content == '4625':
+				print('{}Fail connection (type {} - {}) of {}\\{} from {} ({}) with LogonProcess {} ({})'.format(header, hit.event.EventData.LogonType, LogonHistory.LOGON_SUBSTATUS.get(hit.event.EventData.SubStatus.lower(), hit.event.EventData.SubStatus.lower()),  hit.event.EventData.TargetDomainName, hit.event.EventData.TargetUserName, hit.event.EventData.IpAddress, hit.event.EventData.WorkstationName, hit.event.EventData.LogonProcessName, hit.event.EventData.ProcessName))
+				
+
 		
 
 class StatLogon(ElasticScenario):
